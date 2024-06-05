8ff51f219864eada5363d3e15ca1c6b75a9451f9
Muvva123 <hanumantharao.muvva@gmail.com>
2024-06-05 19:15:42 +0530

    commit

---
+++ modules/data-actions/variables.tf
@@ -0,0 +1,6 @@
+
+variable "data_actions_integration_name" {
+    type        = string
+    description = "Name of the Genesys Cloud data actions integration."
+    default     = "Genesys Cloud Data Actions"
+}
