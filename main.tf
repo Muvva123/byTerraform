89597fdcce67030bc6ad2488ef7d65d3b88ca408
Muvva123 <hanumantharao.muvva@gmail.com>
2024-06-06 17:42:42 +0530

    commit

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "check_queue_flow" {
     source = "./modules/check-queue-flow"
 
     archy_flow_file            = "archy_flow.yml"
-    did_numbers                = ["+12345678926"]
+    did_numbers                = ["+12345678928"]
     primary_queue_member_ids   = []
     secondary_queue_member_ids = []
 }
