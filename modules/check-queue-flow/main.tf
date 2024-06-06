d2525dd058dcbf8cd7b2d8e3cd6a040ac3c6673c
Muvva123 <hanumantharao.muvva@gmail.com>
2024-06-06 16:24:24 +0530

    commit

--- modules/check-queue-flow/main.tf
+++ modules/check-queue-flow/main.tf
@@ -13,14 +13,14 @@ module "check_queue_data_action" {
 
 module "primary_queue" {
     source           = "../queues"
-    name             = "Primary Queue Example6"
+    name             = "Primary Queue Example7"
     description      = "The primary queue called from 'Queue Members Check' inbound call flow."
     queue_member_ids = var.primary_queue_member_ids
 }
 
 module "secondary_queue" {
     source           = "../queues"
-    name             = "Secondary Queue Example6"
+    name             = "Secondary Queue Example7"
     description      = "The secondary queue called from 'Queue Members Check' inbound call flow."
     queue_member_ids = var.secondary_queue_member_ids
 }
